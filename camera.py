d9a856883e560319cb8c9204d58e4158cdb7f0c9
AbdelrahmanElsherif <abdelrhmantawfiek20@yahoo.com>
2020-04-04 00:23:38 +0200

    camera.py

---
+++ camera.py
@@ -0,0 +1,26 @@
+import cv2
+
+
+def show_webcam(mirror=False):
+    cap = cv2.VideoCapture(0)
+    cv2.namedWindow('Webcam Life2Coding', cv2.WINDOW_AUTOSIZE)
+    while True:
+        ret_val, frame = cap.read()
+
+        if mirror:
+            frame = cv2.flip(frame, 1)
+
+        cv2.imshow('Webcam Life2Coding', frame)
+
+        if cv2.waitKey(1) == 27:
+            break  # esc to quit
+
+    cv2.destroyAllWindows()
+
+
+def main():
+    show_webcam(mirror=True)
+
+
+if __name__ == '__main__':
+    main()
